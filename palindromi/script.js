1c5352821b40647083352ff43d32fd6fca976c9e
Alex93pa <Ale.pug2@gmail.com>
2021-04-14 16:14:47 +0200

    palindroma fine

---
+++ palindromi/script.js
@@ -0,0 +1,29 @@
+var parola = prompt("inserisci una parola palindroma");
+parola = parola.toLowerCase();
+
+Palindrome(parola);
+
+
+function Palindrome() {
+
+    // converte stringa in un array
+    var arrayValues = parola.split('');
+
+    // legge array al contrario
+    var reverseArrayValues = arrayValues.reverse();
+
+    // converte array in stringa
+    var reverseParola = reverseArrayValues.join('');
+
+    if(parola == reverseParola) {
+        console.log(parola);
+        console.log('La parola inserita è palindroma');
+        document.getElementById('risultato').innerHTML = ("La parola da te inserita '" + parola + "' è palindroma.")
+    }
+    else {
+        console.log(parola);
+        console.log('La parola inserita non è palindroma');
+        document.getElementById('risultato').innerHTML = ("La parola da te inserita '" + parola + "' non è palindroma.")
+    }
+}
+
